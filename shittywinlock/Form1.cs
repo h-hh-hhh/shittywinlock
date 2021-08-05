cc8c24411e4b1242311d12a284137c73367dedf5
h-hh-hhh <t3hwhistler2419@gmail.com>
2021-08-05 23:10:05 +0300

    Add project files.

---
+++ shittywinlock/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shittywinlock {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            label4.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
